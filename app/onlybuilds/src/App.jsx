5e6761dd1fb185a8ac63cba8a6a2c2315479e303
AndreiZ0R <andreiborzagabriel@gmail.com>
2022-12-10 15:23:26 +0200

    done routing

--- app/onlybuilds/src/App.jsx
+++ app/onlybuilds/src/App.jsx
@@ -1,11 +1,32 @@
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <div>
+      <NavBar
+        onSignIn={() => {
+          window.location = "/login";
+        }}
+        onSignUp={() => {
+          window.location = "/signup";
+        }}
+        onHome={() => {
+          window.location = "/";
+        }}
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
